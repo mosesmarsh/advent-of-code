74b9c3c79aaa4ce0106b96b46446f53c5eb633a5
Moses Marsh <mosesmarsh@gmail.com>
2020-03-05 13:30:14 -0800

    day2

--- 2019/solutions.py
+++ 2019/solutions.py
@@ -3,23 +3,48 @@ def load_data(fpath, sep=None):
             open(fpath).read().split(sep)]
 
 
-def day1_1(fpath):
-    with open(fpath) as f:
-        return sum(int(num)//3 - 2
-                   for num in f)
+def day1_1(masses):
+    return sum(int(mass)//3 - 2
+               for mass in masses)
 
-def day1_2(fpath):
-    with open(fpath) as f:
-        tot=0
-        for line in f:
-            num = int(line)
-            cost = num//3 - 2
-            while cost >= 0:
-                tot += cost
-                cost = cost//3 - 2
+def day1_2(masses):
+    tot=0
+    for mass in masses:
+        cost = mass//3 - 2
+        while cost >= 0:
+            tot += cost
+            cost = cost//3 - 2
     return tot
 
-def day2_1(fpath):
-    program = [int(x) for x in
-                open(fpath).read().split(',')]
-    
+def day2_1(prog):
+    ops = {1: int.__add__,
+           2: int.__mul__}
+    for i in range(0,len(prog),4):
+        op_id, x_ind, y_ind, idx = prog[i:i+4]
+        if op_id == 99:
+            return prog
+        x, y = prog[x_ind], prog[y_ind]
+        prog[idx] = ops[op_id](x,y)
+    return "never got to 99"
+
+def run_program(prog):
+    ops = {1: int.__add__,
+           2: int.__mul__}
+    for i in range(0,len(prog),4):
+        op_id, x_ind, y_ind, idx = prog[i:i+4]
+        if op_id == 99:
+            return prog
+        x, y = prog[x_ind], prog[y_ind]
+        prog[idx] = ops[op_id](x,y)
+    return "never got to 99"
+
+def day2_2(prog, target):
+    out = 0
+    for a in range(100):
+        for b in range(100):
+            p = prog.copy()
+            p[1:3] = a, b
+            out = run_program(p)[0]
+            if out==target:
+                return (100*a) + b
+    return 'never got there'
